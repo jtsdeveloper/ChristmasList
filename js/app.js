913aa6fb58db07f87a79c3c026b4b949ded24a2d
jtsdeveloper <jtsdeveloper@gmail.com>
2015-12-22 17:25:14 -0500

    Basic functionality for displaylists controller

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ui.router'])
+var app = angular.module('app', ['ui.router'])
 
   .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider,$stateProvider){
       $urlRouterProvider.otherwise('/');
@@ -20,4 +20,89 @@ angular.module('app', ['ui.router'])
         })
   }])
 
-  
+  app.controller('giftsController', function(){
+    this.recipients = persons;
+  })
+
+
+var persons = [
+
+  {
+    name: "Jeremy",
+    wishlist: [
+      {
+        gift1: "basketball",
+        isBought: false
+      },
+      {
+        gift2: "snacks",
+        isBought: true
+      },
+      {
+        gift3: "shoes",
+        isBought: false
+      },
+      {
+        gift4: "DVD",
+        isBought: false
+      },
+      {
+        gift5: "truck",
+        isBought: false
+      }
+    ]
+  },
+
+  {
+    name: "Karl",
+    wishlist: [
+      {
+        gift1: "basketball",
+        isBought: false
+      },
+      {
+        gift2: "snacks",
+        isBought: true
+      },
+      {
+        gift3: "shoes",
+        isBought: false
+      },
+      {
+        gift4: "DVD",
+        isBought: false
+      },
+      {
+        gift5: "truck",
+        isBought: false
+      }
+    ]
+  },
+
+  {
+    name: "Malone",
+    wishlist: [
+      {
+        gift1: "basketball",
+        isBought: false
+      },
+      {
+        gift2: "snacks",
+        isBought: true
+      },
+      {
+        gift3: "shoes",
+        isBought: false
+      },
+      {
+        gift4: "DVD",
+        isBought: false
+      },
+      {
+        gift5: "truck",
+        isBought: false
+      }
+    ]
+  }
+
+]
